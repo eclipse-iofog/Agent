70991f4ebd42ee4f49872beeb11a251efaa73945
EtienneRacine <etienne@iotracks.com>
2017-03-15 07:42:52 -0700

    Added appropriate header also testing direct Push from Eclipse.

--- client/src/org/eclipse/iofog/Client.java
+++ client/src/org/eclipse/iofog/Client.java
@@ -8,6 +8,19 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 
+/*******************************************************************************
+ * Copyright (c) 2016 Iotracks, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Saeid Baghbidi
+ *   Kilton Hopkins
+ *   Ashita Nagar
+ *******************************************************************************/
+
 public class Client {
 
 	/**
