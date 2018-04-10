31792a124f2447b03b4a990505bb5be86382078f
MaksimChepelev <chepelev.maksim@gmail.com>
2018-04-10 15:56:34 +0300

    refactoring

--- daemon/src/org/eclipse/iofog/gps/GpsMode.java
+++ daemon/src/org/eclipse/iofog/gps/GpsMode.java
@@ -3,24 +3,15 @@ package org.eclipse.iofog.gps;
 import java.util.Arrays;
 
 public enum GpsMode {
-	AUTO("auto"),
-	MANUAL("manual"),
-	OFF("off"),
-	DYNAMIC("dynamic");
+	AUTO,
+	MANUAL,
+	OFF,
+	DYNAMIC;
 
-	private final String value;
-
-	GpsMode(String value) {
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
 
 	public static GpsMode getModeByValue(String command) {
 		return Arrays.stream(GpsMode.values())
-				.filter(gpsMode -> gpsMode.value.equals(command))
+				.filter(gpsMode -> gpsMode.name().toLowerCase().equals(command))
 				.findFirst()
 				.orElseThrow(IllegalArgumentException::new);
 	}
