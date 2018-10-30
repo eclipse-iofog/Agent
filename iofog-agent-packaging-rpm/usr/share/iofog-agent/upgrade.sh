4add88c398c65ebb904a9f57e00f58460176b79f
Railag <whenthegroundcavedin@gmail.com>
2018-10-30 15:17:03 +0300

    renaming

---
+++ iofog-agent-packaging-rpm/usr/share/iofog-agent/upgrade.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+provisionkey=$1
+timeout=${2:-60}
+
+cd /var/backups/iofog-agent
+tar -cvzf config_backup.tar.gz -P /etc/iofog-agent
+
+iofogversion=$(yum list iofog-agent-dev | grep iofog | awk '{print $2}' | sed -n 1p)
+
+printf 'ver: '$iofogversion > prev_version_data
+
+iofog-agent deprovision
+service iofog-agent stop
+
+yum check-update
+yum update iofog-agent-dev -y
+
+starttimestamp=$(date +%s)
+service iofog-agent start
+sleep 1
+
+while [ "$(iofog-agent status | grep ioFog | awk '{printf $4 }')" != "RUNNING" ]; do
+	sleep 1
+	currenttimestamp=$(date +%s)
+	currentdeltatime=$(( $currenttimestamp - $starttimestamp ))
+	if [ $currentdeltatime -gt $timeout ]; then
+		break
+	fi
+done
+
+iofog-agent provision $provisionkey
--- iofog-packaging-rpm/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf /etc/iofog
-rm -rf /var/log/iofog
-rm -rf /var/lib/iofog
-rm -rf /var/run/iofog
-#rm -rf /var/backups/iofog
-rm -rf /usr/share/iofog
