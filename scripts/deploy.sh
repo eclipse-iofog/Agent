cb2a58e4654e399d640d75be00e5c2a41795845c
dbusel <10116634+dbusel@users.noreply.github.com>
2019-01-22 17:30:51 +0300

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BRANCH=$1
+PASSWORD=$2
+USER=$3
+IP=$4
+
+if [ "$BRANCH" != "master" && "$BRANCH" != "develop" ]; then export DEV=-dev; fi
+
+sshpass -p ${PASSWORD} ssh -p StrictHostKeyChecking=no ${USER}@${IP}
+    "service iofog-agent${DEV} stop sudo apt-get install --only-upgrade -y iofog-agent";
+if [ "$BRANCH" == "develop" ]; then sshpass -p ${PASSWORD} scp -p StrictHostKeyChecking=no daemon/target/iofog-agent-daemon-jar-with-dependencies.jar
+    ${USER}@${IP}:/usr/bin/iofog-agentd.jar; sshpass -p ${PASSWORD} scp -p StrictHostKeyChecking=no client/target/iofog-agent-client-jar-with-dependencies.jar
+    ${USER}@${IP}:/usr/bin/iofog-agent.jar; fi
+sshpass -p ${PASSWORD} ssh -p StrictHostKeyChecking=no ${USER}@${IP}
+    "service iofog-agent${DEV} start"
