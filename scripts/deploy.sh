c3bbaaa0f33a99bec085388a20ec10328e86e9f7
dbusel <10116634+dbusel@users.noreply.github.com>
2019-02-05 15:34:39 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ PASSWORD=$1
 USER=$2
 IP=$3
 
-sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-agent stop sudo apt-get install --only-upgrade -y iofog-agent${DEV}"
+sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-agent stop; sudo apt-get install --only-upgrade -y iofog-agent${DEV}"
 if [ "$TRAVIS_BRANCH" == "develop" ]; then 
     sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no daemon/target/iofog-agent-daemon-jar-with-dependencies.jar ${USER}@${IP}:/usr/bin/iofog-agentd.jar; 
     sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no client/target/iofog-agent-client-jar-with-dependencies.jar ${USER}@${IP}:/usr/bin/iofog-agent.jar;
