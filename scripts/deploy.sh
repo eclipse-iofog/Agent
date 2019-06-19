eb7945c15b4973066f2845e5fdcfc60ef2ddd623
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-06-19 17:28:34 +0530

    update release candidate package name

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-PASSWORD=$1
-USER=$2
-IP=$3
-
-sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-agent stop; sudo apt-get update; sudo apt-get install --only-upgrade -y iofog-agent${ID}; service iofog-agent start"
