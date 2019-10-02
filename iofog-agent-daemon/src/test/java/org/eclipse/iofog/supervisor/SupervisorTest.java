186d71199601304da4ade9a8acafa5f4749d6231
Neha Naithani <neha@edgeworx.io>
2019-10-02 20:52:40 +1300

    supervisor package unit test

--- iofog-agent-daemon/src/test/java/org/eclipse/iofog/supervisor/SupervisorTest.java
+++ iofog-agent-daemon/src/test/java/org/eclipse/iofog/supervisor/SupervisorTest.java
@@ -1,24 +1,37 @@
+package org.eclipse.iofog.supervisor;
+
+import org.eclipse.iofog.IOFogModule;
+
+import org.eclipse.iofog.exception.AgentSystemException;
 import org.eclipse.iofog.field_agent.FieldAgent;
 import org.eclipse.iofog.local_api.LocalApi;
 import org.eclipse.iofog.local_api.LocalApiStatus;
 import org.eclipse.iofog.message_bus.MessageBus;
 import org.eclipse.iofog.process_manager.ProcessManager;
 import org.eclipse.iofog.resource_consumption_manager.ResourceConsumptionManager;
+import org.eclipse.iofog.resource_manager.ResourceManager;
 import org.eclipse.iofog.status_reporter.StatusReporter;
-import org.eclipse.iofog.supervisor.Supervisor;
-import org.eclipse.iofog.supervisor.SupervisorStatus;
 import org.eclipse.iofog.tracking.Tracker;
+import org.eclipse.iofog.utils.configuration.Configuration;
 import org.eclipse.iofog.utils.logging.LoggingService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.mockito.Mockito.verify;
+import java.lang.reflect.Method;
+
+import static org.eclipse.iofog.utils.Constants.ModulesStatus.RUNNING;
+import static org.eclipse.iofog.utils.Constants.ModulesStatus.STARTING;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.support.membermodification.MemberMatcher.method;
@@ -27,13 +40,16 @@ import static org.powermock.api.support.membermodification.MemberModifier.suppre
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Supervisor.class, StatusReporter.class, ResourceConsumptionManager.class,
         FieldAgent.class, ProcessManager.class, Tracker.class, SecurityManager.class,
-        MessageBus.class, LocalApi.class, LoggingService.class})
+        MessageBus.class, LocalApi.class, LoggingService.class, Configuration.class})
 public class SupervisorTest {
     private Supervisor supervisor;
+    private Method method = null;
+    private ResourceManager resourceManager;
 
     @Before
     public void initialization() {
         try {
+            supervisor = spy(new Supervisor());
             mockStatic(StatusReporter.class);
             mockStatic(ResourceConsumptionManager.class);
             mockStatic(FieldAgent.class);
@@ -52,6 +68,7 @@ public class SupervisorTest {
             PowerMockito.when(Tracker.getInstance()).thenReturn(new Tracker());
             PowerMockito.when(MessageBus.getInstance()).thenReturn(null);
             // PowerMockito.when(LocalApi.getInstance()).thenReturn(null);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -64,14 +81,6 @@ public class SupervisorTest {
             suppress(method(Supervisor.class, "startModule"));
             suppress(method(Supervisor.class, "operationDuration"));
             supervisor.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @After
-    public void verifyTest() {
-        try {
             verify(supervisor, Mockito.atLeastOnce()).start();
             verify(supervisor, Mockito.never()).getModuleIndex();
             verify(supervisor, Mockito.atLeastOnce()).getModuleName();
@@ -81,5 +90,35 @@ public class SupervisorTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @After
+    public void verifyTest() {
+        if (method != null)
+            method.setAccessible(false);
+    }
+    /**
+     * Test start module
+     */
+    @Test
+    public void testStartModule() throws Exception{
+        resourceManager = mock(ResourceManager.class);
+        PowerMockito.when(resourceManager.getModuleIndex()).thenReturn(6);
+        PowerMockito.when(resourceManager.getModuleName()).thenReturn("ResourceManager");
+        PowerMockito.when(StatusReporter.setSupervisorStatus().setModuleStatus(6, STARTING)).thenReturn(mock(SupervisorStatus.class));
+        PowerMockito.when(StatusReporter.setSupervisorStatus().setModuleStatus(6, RUNNING)).thenReturn(null);
+        method = Supervisor.class.getDeclaredMethod("startModule", IOFogModule.class);
+        method.setAccessible(true);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return null;
+            }
+        }).
+                when(resourceManager).start();
+        method.invoke(supervisor, resourceManager);
+        verify(supervisor, Mockito.atLeastOnce()).logInfo(" Starting ResourceManager");
+        verify(supervisor, Mockito.atLeastOnce()).logInfo(" Started ResourceManager");
     }
 }
