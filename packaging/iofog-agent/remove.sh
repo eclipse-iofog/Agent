f99a714f99f7347b9f8598b37130bd5a513bd1cd
SergeRadinovich <serge@edgeworx.io>
2020-10-13 11:34:05 +1300

    Create /var/log/iofog-microservices in apt install

--- packaging/iofog-agent/remove.sh
+++ packaging/iofog-agent/remove.sh
@@ -4,7 +4,7 @@ rm -rf /etc/iofog-agent
 rm -rf /var/log/iofog-agent
 rm -rf /var/lib/iofog-agent
 rm -rf /var/run/iofog-agent
-#rm -rf /var/backups/iofog-agent
+rm -rf /var/log/iofog-microservices
 rm -rf /usr/share/iofog-agent
 
 containers=$(docker ps | grep iofog_ | awk --posix '{print $1}')
