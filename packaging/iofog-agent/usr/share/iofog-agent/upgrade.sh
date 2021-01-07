7d3bf3f4b9a7f5529f5a11b4c64dad96a60a2b8e
Neha Naithani <neha@edgeworx.io>
2021-01-07 17:13:42 +1300

    Stop agent before starting backup for upgrade

--- packaging/iofog-agent/usr/share/iofog-agent/upgrade.sh
+++ packaging/iofog-agent/usr/share/iofog-agent/upgrade.sh
@@ -23,14 +23,16 @@ get_distribution() {
 	BACKUP="/var/backups/iofog-agent/config.xml"
 	cp "$ORIGINAL" "$BACKUP"
 
+	# Stop agent
+	service iofog-agent stop
+
 	# Create backup for rollback
 	cd /var/backups/iofog-agent
 	tar -cvzf config_backup$iofogpackage.tar.gz -P /etc/iofog-agent
 	tar -cvzf log_backup_upgrade$iofogversion.tar.gz -P /var/log/iofog-agent
 	printf 'ver: %s %s' $iofogversion $iofogpackage > prev_version_data
 
-	# Stop agent
-	service iofog-agent stop
+	# remove current configs
 	rm /etc/iofog-agent/*
 
 	# Perform upgrade
