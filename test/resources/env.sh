4b6684b90c93b8b8eba5e1e28f211697a3f9b11f
NehaNaithani <neha@edgeworx.io>
2023-04-20 16:42:50 +0530

    Added integration tests

--- test/resources/env.sh
+++ test/resources/env.sh
@@ -1,4 +1,4 @@
 export USAGE="5"
-export AGENT_IMAGE="gcr.io/focal-freedom-236620/agent:latest"
+export AGENT_IMAGE="ghcr.io/eclipse-iofog/agent:latest"
 export CONTROLLER_IMAGE="iofog/controller:latest"
 export NAMESPACE="int-test"
